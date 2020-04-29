8f37cd2cb9cd7910ca70c3b1b4116934aa5a1713
Eliza Coroiu <ecoroiu31@gmail.com>
2020-04-29 21:57:20 +0300

    Initial commit

---
+++ POO-SortareLocalizata/POO-SortareLocalizata/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace POO_SortareLocalizata
+{
+    internal class Person
+    {
+        private string line;
+        private string[] names;
+
+        public Person(string line)
+        {
+            this.line = line;
+            char[] seps = { ' ', ',' };
+            names = line.Split(seps, StringSplitOptions.RemoveEmptyEntries);
+
+            CultureInfo myCIintl = new CultureInfo("ro-RO", false);
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                string allLower = names[i].ToLower(myCIintl);
+
+                names[i] = $"{char.ToUpper(allLower[0], myCIintl)}{allLower.Substring(1)}";
+            }
+        }
+
+        public override string ToString()
+        {
+            string firstName = "";
+
+            for (int i = 1; i < names.Length; i++)
+            {
+                firstName += names[i] + " ";
+            }
+           
+            return $"{names[0]} {firstName}";
+        }    
+    }
+}
+        
+
+        
+            
+
+        
---
+++ POO-SortareLocalizata/POO-SortareLocalizata/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace POO_SortareLocalizata
+{
+    internal class MyComparer : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return string.Compare(x.ToString(), y.ToString(), new CultureInfo("ro-RO"), CompareOptions.None);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                List<Person> listOfPersons = new List<Person>();
+                string fileName = @"..\..\nume.txt";
+
+                StreamReader sr = new StreamReader(fileName);
+                string line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Person currentPerson = new Person(line);
+                    listOfPersons.Add(currentPerson);
+
+                    line = sr.ReadLine();
+                }
+
+                Console.WriteLine("Before sorting: ");
+                Console.WriteLine();
+                foreach (var persona in listOfPersons)
+                {
+                    Console.WriteLine(persona);
+                }
+
+                listOfPersons.Sort(new MyComparer());
+
+                Console.WriteLine();
+                Console.WriteLine("After sorting: ");
+                Console.WriteLine();
+
+                foreach (var person in listOfPersons)
+                {
+                    Console.WriteLine(person);
+                }
+
+                fileName = @"..\..\output.txt";
+                StreamWriter sw = new StreamWriter(fileName);
+
+                foreach (var persona in listOfPersons)
+                {
+                    sw.WriteLine(persona);
+                }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("This file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
+
