8a27ea5594da8da054b04b5e6351e8f6347d45e2
Eliza Coroiu <ecoroiu31@gmail.com>
2020-05-14 13:47:59 +0300

    MyStack Project

---
+++ MyStack/MyStack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stiva<int> st1 = new Stiva<int>(4);
+
+            st1.Push(4);
+            st1.Push(3);
+            st1.Push(2);
+            st1.Push(1);
+
+            Console.WriteLine("Stack of integers: ");
+            PopAllElements(st1);
+
+            Stiva<string> st2 = new Stiva<string>(10);
+            st2.Push("August");
+            st2.Push("July");
+            st2.Push("June");
+            st2.Push("May");
+            st2.Push("April");
+            st2.Push("March");
+
+            Console.WriteLine("Stack of strings: ");
+            PopAllElements(st2);
+            
+            st2.Clear();
+            st2.Pop();
+
+            Console.ReadKey();
+        }
+
+        private static void PopAllElements<T>(Stiva<T> st)
+        {
+            while (!st.Empty)
+            {
+                Console.WriteLine(st.Pop());
+            }
+        }
+    }
+}
---
+++ MyStack/MyStack/Stiva.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MyStack
+{
+    public class Stiva<T> 
+    {
+        private T[] elements;
+        private int currentPosition = 0;
+
+        public bool Empty
+        {
+            get
+            {
+                if (currentPosition != 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+        public Stiva(int maxCount)
+        {
+            elements = new T[maxCount];
+        }
+
+        public void Push(T x)
+        {
+            if (currentPosition < elements.Length)
+            {
+                elements[currentPosition++] = x;
+            }
+            else
+            {
+                throw new StackFullException("Stack is full!");
+            }
+        }
+
+        public T Pop()
+        {
+            if (!this.Empty)
+            {
+                currentPosition--;
+                return elements[currentPosition];
+            }
+            else
+            {
+                throw new StackEmptyException("Stack is empty!");
+            }
+        }
+
+        public void Clear()
+        {
+            elements = new T[elements.Length];
+        }
+    }
+}
